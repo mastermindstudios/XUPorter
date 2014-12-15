31cdb1dbceace0c73a36152fdbab72dfba83909e
Tim Robles <tim@mastermindstudios.net>
2014-12-15 14:27:11 -0800

    add meta files

--- XCodePostProcess.cs
+++ XCodePostProcess.cs
@@ -9,37 +9,37 @@ using System.IO;
 public static class XCodePostProcess
 {
 
-#if UNITY_EDITOR
-	[PostProcessBuild(999)]
-	public static void OnPostProcessBuild( BuildTarget target, string pathToBuiltProject )
-	{
-		if (target != BuildTarget.iPhone) {
-			Debug.LogWarning("Target is not iPhone. XCodePostProcess will not run");
-			return;
-		}
+//#if UNITY_EDITOR
+//    [PostProcessBuild(999)]
+//    public static void OnPostProcessBuild( BuildTarget target, string pathToBuiltProject )
+//    {
+//        if (target != BuildTarget.iPhone) {
+//            Debug.LogWarning("Target is not iPhone. XCodePostProcess will not run");
+//            return;
+//        }
 
-		// Create a new project object from build target
-		XCProject project = new XCProject( pathToBuiltProject );
+//        // Create a new project object from build target
+//        XCProject project = new XCProject( pathToBuiltProject );
 
-		// Find and run through all projmods files to patch the project.
-		// Please pay attention that ALL projmods files in your project folder will be excuted!
-		string[] files = Directory.GetFiles( Application.dataPath, "*.projmods", SearchOption.AllDirectories );
-		foreach( string file in files ) {
-			UnityEngine.Debug.Log("ProjMod File: "+file);
-			project.ApplyMod( file );
-		}
+//        // Find and run through all projmods files to patch the project.
+//        // Please pay attention that ALL projmods files in your project folder will be excuted!
+//        string[] files = Directory.GetFiles( Application.dataPath, "*.projmods", SearchOption.AllDirectories );
+//        foreach( string file in files ) {
+//            UnityEngine.Debug.Log("ProjMod File: "+file);
+//            project.ApplyMod( file );
+//        }
 
-		//TODO implement generic settings as a module option
-		project.overwriteBuildSetting("CODE_SIGN_IDENTITY[sdk=iphoneos*]", "iPhone Distribution", "Release");
+//        //TODO implement generic settings as a module option
+//        project.overwriteBuildSetting("CODE_SIGN_IDENTITY[sdk=iphoneos*]", "iPhone Distribution", "Release");
 		
-		// Finally save the xcode project
-		project.Save();
+//        // Finally save the xcode project
+//        project.Save();
 
-	}
-#endif
+//    }
+//#endif
 
-	public static void Log(string message)
-	{
-		UnityEngine.Debug.Log("PostProcess: "+message);
-	}
+//    public static void Log(string message)
+//    {
+//        UnityEngine.Debug.Log("PostProcess: "+message);
+//    }
 }
